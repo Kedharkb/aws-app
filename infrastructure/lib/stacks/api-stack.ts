a9069ef3ecf12e3328ab2edcb24d304edb2f74a6
Kedharkb <kedhar.kb@gmail.com>
2024-03-15 17:13:21 +0100

    lambda role update

--- infrastructure/lib/stacks/api-stack.ts
+++ infrastructure/lib/stacks/api-stack.ts
@@ -2,23 +2,26 @@ import * as cdk from 'aws-cdk-lib';
 import {Duration} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Function, InlineCode, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
-import {Role,ServicePrincipal} from 'aws-cdk-lib/aws-iam'
+import {Role,ServicePrincipal, PolicyStatement} from 'aws-cdk-lib/aws-iam'
 
 export class ApiStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
 
-    const myRole = new Role(this, 'My Role', {
-        assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
-      });
+    // const myRole = new Role(this, 'lambda_deploy', {
+    //     assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+    //   });
       
+    const existingRole = Role.fromRoleArn(this, 'LambdaRole', 'arn:aws:iam::381492264897:role/3814-9226-4897');
+
 
     new Function(this, 'ApiLambda', {
         runtime: Runtime.PYTHON_3_10,
         handler: 'app.lambda_handler',
         code: Code.fromAsset('../src/lambdas/api_lambda/api_lambda'),
-        role:myRole,
+        role:existingRole,
         timeout: Duration.minutes(5)
       });
     }
 }
+

2b2b325247765d7eed061e63c1e7450b33b0ca32
Kedharkb <kedhar.kb@gmail.com>
2024-03-17 16:44:01 +0100

    entry updated

--- infrastructure/lib/stacks/api-stack.ts
+++ infrastructure/lib/stacks/api-stack.ts
@@ -7,19 +7,12 @@ export class ApiStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
     
-      const pythonlayer = new LayerVersion(this,'PythonLayer',{
-        code: Code.fromAsset('../src/lambdas/api_lambda/'),
-        layerVersionName: `api-lambda-layer`,
-        description: "Layer for api Lambda python dependencies",
-        compatibleRuntimes: [Runtime.PYTHON_3_10],
-      })
 
     new Function(this, 'ApiLambda', {
         runtime: Runtime.PYTHON_3_10,
-        handler: 'api_lambda.app.lambda_handler',
+        handler: 'main.lambda_handler',
         code: Code.fromAsset('../src/lambdas/api_lambda/'),
         timeout: Duration.minutes(5),
-        layers:[pythonlayer]
       });
     }
 }
---
+++ src/lambdas/api_lambda/main.py
@@ -0,0 +1,3 @@
+from api_lambda.app import lambda_handler
+if __name__ == "__main__":
+    lambda_handler({}, {})

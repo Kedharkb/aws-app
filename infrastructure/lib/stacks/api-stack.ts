fe625585d3a596f8bf4ae49cbf0b659e5c1f79ef
Kedharkb <kedhar.kb@gmail.com>
2024-03-17 17:11:27 +0100

    python function cdk updated

--- infrastructure/lib/stacks/api-stack.ts
+++ infrastructure/lib/stacks/api-stack.ts
@@ -2,16 +2,19 @@ import * as cdk from 'aws-cdk-lib';
 import {Duration} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Function, InlineCode, Runtime, Code, LayerVersion } from 'aws-cdk-lib/aws-lambda';
+import { PythonFunction } from "@aws-cdk/aws-lambda-python-alpha";
 
 export class ApiStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
     
 
-    new Function(this, 'ApiLambda', {
+    new PythonFunction(this, 'ApiLambda', {
         runtime: Runtime.PYTHON_3_10,
-        handler: 'main.lambda_handler',
-        code: Code.fromAsset('../src/lambdas/api_lambda/'),
+        handler: 'lambda_handler',
+        // code: Code.fromAsset('../src/lambdas/api_lambda/'),
+        entry: "../src/lambdas/api_lambda",
+        index: "main.py",
         timeout: Duration.minutes(5),
       });
     }
